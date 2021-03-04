a6f6720b526d2500e4ae9e60b437bdb9e18ac810
Yury <ymorpheus@gmail.com>
2021-03-04 23:34:43 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min(array) {
-  return 0;
+  if (!array || !Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+
+  return Math.min(...array);
 }
 
 exports.max = function max(array) {
-  return 0;
+  if (!array || !Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+
+  return Math.max(...array);
 }
 
 exports.avg = function avg(array) {
-  return 0;
-}
+  if (!Array.isArray(array) || !array || array.length === 0) {
+    return 0;
+  }
+
+  return array.reduce((el, acc) => el + acc) / array.length
+} 
